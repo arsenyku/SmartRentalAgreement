dc8ca7a1db17b641bd4c27140e98c3d0fdf4eb77
asu <arsenyk.ustaris.dev@gmail.com>
2017-01-14 15:53:00 -0800

    Additional endpoint data

--- SmartRentalAgreement/Data/SmartRentClient.swift
+++ SmartRentalAgreement/Data/SmartRentClient.swift
@@ -11,6 +11,9 @@ import Foundation
 class SmartRentClient
 {
   static var propertyData:[String:Any]? = nil
+  static var contractData:[String:Any]? = nil
+  static var paymentData:[String:Any]? = nil
+  static var terminateData:[String:Any]? = nil
   
   func requestHistory(forProperty:String, completion: @escaping (Dictionary<String,Any>) -> Void) -> Void
   {
@@ -21,10 +24,57 @@ class SmartRentClient
         return
       }
       
+      SmartRentClient.propertyData = jsonDictionary
+
       completion(jsonDictionary)
       
-      SmartRentClient.propertyData = jsonDictionary
     })
   }
 
+  
+  func requestPaymentData(forProperty:String, completion: @escaping (Dictionary<String,Any>) -> Void) -> Void
+  {
+    let _ = URLSession.download(fromAddress: "http://10.10.42.198:3000/payment", completionHandler: { (data, response, error) -> Void in
+      
+      guard let json = try? JSONSerialization.jsonObject(with: data!, options: []),
+        let jsonDictionary = json as? [String:Any] else {
+          return
+      }
+      
+      SmartRentClient.paymentData = jsonDictionary
+      
+      completion(jsonDictionary)
+    })
+  }
+  func requestContractData(forProperty:String, completion: @escaping (Dictionary<String,Any>) -> Void) -> Void
+  {
+    let _ = URLSession.download(fromAddress: "http://10.10.42.198:3000/contract", completionHandler: { (data, response, error) -> Void in
+      
+      guard let json = try? JSONSerialization.jsonObject(with: data!, options: []),
+        let jsonDictionary = json as? [String:Any] else {
+          return
+      }
+      
+      SmartRentClient.contractData = jsonDictionary
+      
+      completion(jsonDictionary)
+    })
+  }
+  func requestTerminateData(forProperty:String, completion: @escaping (Dictionary<String,Any>) -> Void) -> Void
+  {
+    let _ = URLSession.download(fromAddress: "http://10.10.42.198:3000/terminate", completionHandler: { (data, response, error) -> Void in
+      
+      guard let json = try? JSONSerialization.jsonObject(with: data!, options: []),
+        let jsonDictionary = json as? [String:Any] else {
+          return
+      }
+      
+      SmartRentClient.terminateData = jsonDictionary
+      
+      completion(jsonDictionary)
+    })
+  }
+
+  
+  
 }
