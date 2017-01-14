ff4c7c68195429b170820ba9021b89c0dde7adc1
asu <arsenyk.ustaris.dev@gmail.com>
2017-01-14 13:07:36 -0800

    Initial

---
+++ SmartRentalAgreement/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SmartRentalAgreement
+//
+//  Created by asu on 2017-01-14.
+//  Copyright © 2017 SmartRentalAgreement. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ SmartRentalAgreement/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SmartRentalAgreement
+//
+//  Created by asu on 2017-01-14.
+//  Copyright © 2017 SmartRentalAgreement. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
