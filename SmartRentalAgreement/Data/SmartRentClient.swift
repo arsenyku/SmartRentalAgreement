d93ce5219b29e494dc339b12abbb0468956fc387
asu <arsenyk.ustaris.dev@gmail.com>
2017-01-14 14:55:09 -0800

    Add network functions

---
+++ SmartRentalAgreement/Data/SmartRentClient.swift
@@ -0,0 +1,14 @@
+//
+//  SmartRentClient.swift
+//  SmartRentalAgreement
+//
+//  Created by asu on 2017-01-14.
+//  Copyright © 2017 SmartRentalAgreement. All rights reserved.
+//
+
+import Foundation
+
+func requestHistory(forTenant tenant:String, completion: @escaping (Dictionary<String,String>) -> Void)
+{
+  
+}
