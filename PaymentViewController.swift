451f715ae88a851a57c55b46d9153f198c794fff
Thomas Minshull <thomasminshull@gmail.com>
2017-01-14 17:44:20 -0800

    total hack

--- PaymentViewController.swift
+++ PaymentViewController.swift
@@ -31,27 +31,29 @@ class PaymentViewController: UIViewController {
     }
 
     @IBAction func submitPaymentTapped(_ sender: Any) {
-        let urlBaseString = "10.10.42.198:3000/make_payment"
-        let amountString = "?amount="
-        let url = "\(urlBaseString)\(textField.text)"
+//        let urlBaseString = "10.10.42.198:3000/make_payment"
+//        let amountString = "?amount="
+//        let url = "\(urlBaseString)\(textField.text)"
+//        
+//        var urlComponents = NSURLComponents(string: "10.10.42.198:3000/payment")
+//        urlComponents?.queryItems = [
+//            NSURLQueryItem(name: "amount", value: textField.text) as URLQueryItem,
+//            NSURLQueryItem(name: "id", value: "1") as URLQueryItem
+//        ]
+//        let url2 = urlComponents?.url
+//        
+//        DispatchQueue.global(qos: .background).async {
+//            //var request = URLRequest(url: URL(string: urlBaseString)!)
+//            var request = URLRequest(url: url2!)
+//            request.httpMethod = "GET"
+//            
+//            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+//                print("data: \(data), response: \(response), error: \(error)")
+//            }
+//            task.resume()
+        currentBalanceLabel.text = textField.text
         
-        var urlComponents = NSURLComponents(string: "10.10.42.198:3000/payment")
-        urlComponents?.queryItems = [
-            NSURLQueryItem(name: "amount", value: textField.text) as URLQueryItem,
-            NSURLQueryItem(name: "id", value: "1") as URLQueryItem
-        ]
-        let url2 = urlComponents?.url
         
-        DispatchQueue.global(qos: .background).async {
-            //var request = URLRequest(url: URL(string: urlBaseString)!)
-            var request = URLRequest(url: url2!)
-            request.httpMethod = "GET"
-            
-            let task = URLSession.shared.dataTask(with: request) { data, response, error in
-                print("data: \(data), response: \(response), error: \(error)")
-            }
-            task.resume()
-            
         }
         
         
