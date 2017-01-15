617bc465b96d204d9c6b42eec921a9ab9d95fd58
asu <arsenyk.ustaris.dev@gmail.com>
2017-01-14 16:15:06 -0800

    Constraints and Custom History Cell

--- SmartRentalAgreement/PropertyHistoryViewController.swift
+++ SmartRentalAgreement/PropertyHistoryViewController.swift
@@ -10,12 +10,18 @@ import UIKit
 
 class PropertyHistoryViewController: UIViewController {
 
+  @IBOutlet weak var tableView: UITableView!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+      print ("will appear")
+    }
+  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
